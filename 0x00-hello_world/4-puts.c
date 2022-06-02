80c01cc10ae436b6a32edca1f493819e319e0743
Miss24Chimboto <annastacia78@gmail.com>
2022-06-02 13:11:22 -0700

    Printing a C file using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Added the library
+ * Main void enables the code to run successfully
+ *
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
+
