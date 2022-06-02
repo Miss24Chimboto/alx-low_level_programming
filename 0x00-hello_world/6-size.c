b5c09f5a2cc742383fc7d628aeee1b08badbed7d
Miss24Chimboto <annastacia78@gmail.com>
2022-06-02 15:31:33 -0700

    Printing the sizes of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  *Description: prints the sizes of various types)?
  *Return: 0
  */
-int main (void)
+int main(void)
 {
  char sizechar;
  int  sizeint;
@@ -14,11 +14,11 @@ int main (void)
  long long sizelonglong;
  float sizefloat;
 
-printf("Size of char:%lu\n", sizeof(sizechar));
-printf("Size of int:%lu\n", sizeof(sizeint));
-printf("Size of long:%lu\n", sizeof(sizelong));
-printf("Size of longlong:%lu\n", sizeof(sizelonglong));
-printf("Size of float:%lu\n", sizeof(sizefloat));
+printf("Size of a char:%lu byte(s)\n", sizeof(sizechar));
+printf("Size of an int:%lu byte(s)\n", sizeof(sizeint));
+printf("Size of a long:%lu byte(s)\n", sizeof(sizelong));
+printf("Size of a longlong:%lubyte(s)\n", sizeof(sizelonglong));
+printf("Size of a float:%lu byte(s)\n", sizeof(sizefloat));
 
 return (0);
 }
