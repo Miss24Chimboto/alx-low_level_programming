39d0eeb915086defb6e9fa46982a6ca8624f3dac
Miss24Chimboto <annastacia78@gmail.com>
2022-06-02 13:46:04 -0700

    Using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - void enables the code to run successfully
  (*
  *Description: prints using the put)?
- *Return: 0
+ *Return 0
  */
 int main(void)
 {
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+/**
+ * main- printf runs the program
+ (*
+ *Description: void enables the program to run)?
+ *Return: 0
+ */ 
+main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
