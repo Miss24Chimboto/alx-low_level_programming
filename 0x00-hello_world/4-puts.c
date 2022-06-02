9c21fa942ed17b147eb316e144c1f601f681ddd4
Miss24Chimboto <annastacia78@gmail.com>
2022-06-02 13:24:23 -0700

    Changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
  * Added the library
- * Main void enables the code to run successfully
- *
+ * main - void enables the code to run successfully
+ (*
+ *Description: prints using the put)?
+ *Return: 0 
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
 
