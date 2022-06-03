7aeb9482d916b03b6065ad7e8579fd2fd863fa95
Miss_Chimboto <annastacia78@gmail.com>
2022-06-03 22:53:13 +0300

    The alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* This prints the alphabet */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+		putchar('\n');
+
+	return (0);
+}
