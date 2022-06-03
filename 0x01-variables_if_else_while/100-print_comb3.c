afd07795c256b80ae3e1edb433a1ee2a0f31524e
Miss_Chimboto <annastacia78@gmail.com>
2022-06-03 23:58:34 +0300

    Digit combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 	i = 48;
 	e = 48;
 
-	while (e < 58)
+while (e < 58)
 	{
 i = 48;
 while (i < 58)
@@ -22,7 +22,7 @@ putchar(e);
 putchar(i);
 if (i == 57 && e == 56)
 {
-    break;
+break;
 }
 putchar(',');
 putchar(' ');
