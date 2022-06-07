729a68578cebfb4e4c6433cd97b926c0d940508c
Miss_Chimboto <annastacia78@gmail.com>
2022-06-08 00:51:29 +0300

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * prints out _putchar followed by a newline.
+ *
+ * Return: 0 
+ */
+
+int main(void)
+{
+	char ch[8] = "_putchar";
+	int p;
+
+	for (p = 0; p < 8; p++)
+		_putchar(ch[p]);
+
+	_putchar('\n');
+
+	return (0);
+}
