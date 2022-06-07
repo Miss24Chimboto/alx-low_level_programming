e1352a7b51fa9a3ebea3887a9dc4231d7df96890
Miss_Chimboto <annastacia78@gmail.com>
2022-06-08 01:18:09 +0300

    Lowercase alphabet function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
+ * print_alphabet prints alphabets in lowercase
  * main - enables it to run successfully
  *
  * Description: function that prints_alphabet in lowercase
  *
  * Return: 0
  */
-
 void print_alphabet(void)
 {
 char low;
