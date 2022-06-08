fe73aac8aa87fa27d1607980b899c2a275145bc0
Miss_Chimboto <annastacia78@gmail.com>
2022-06-08 03:49:43 +0300

    Lowercase or uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isalpha checks for lower/uppercase character
+ * _isalpha - checks for lower/uppercase character
  * @c: the character to check
  * Description: function that prints the alphabet
  * Return: 1 if c is a letter, lowercase or uppercase
