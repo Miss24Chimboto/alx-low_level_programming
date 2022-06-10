015cd765d2e1480df70e8877bcd8c5c95d9e6b40
Miss_Chimboto <annastacia78@gmail.com>
2022-06-10 18:57:22 +0300

    Printing integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,7 +6,6 @@
 void print_number(int n)
 {
 unsigned int m, d, count;
-
 if (n < 0)
 {
 _putchar(45);
@@ -16,7 +15,6 @@ else
 {
 m = n;
 }
-
 d = m;
 count = 1;
 
@@ -25,7 +23,6 @@ while (d > 9)
 d /= 10;
 count *= 10;
 }
-
 for (; count >= 1; count /= 10)
 {
 _putchar(((m / count) % 10) + 48);
