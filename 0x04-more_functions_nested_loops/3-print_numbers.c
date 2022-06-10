c1c8d58fb1a096a9f290d3e7563e6e94403edfca
Miss_Chimboto <annastacia78@gmail.com>
2022-06-10 15:19:33 +0300

    function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_most_numbers -  prints the numbers except 2 and 4.
+*/
+void print_numbers(void);
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	
+    _putchar("%d", a);
+	_putchar('\n');
+}
