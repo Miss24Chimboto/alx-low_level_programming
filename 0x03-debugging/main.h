806f97bde1eab1c4e56d9a1a926a06c14ff41787
Miss24Chimboto <46428707+Miss24Chimboto@users.noreply.github.com>
2022-06-11 15:40:45 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-2-largest_number(int);
-print_remaining_days(int);
-convert_day
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 #endif /* MAIN_H */
