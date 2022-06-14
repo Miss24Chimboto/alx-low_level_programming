0d84b86274c8231760f1dfb9f085f7995e714162
Miss24Chimboto <annastacia78@gmail.com>
2022-06-14 16:50:28 -0700

    Printint string characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * puts2 - a function that prints every other character of a string,
- * starting with the first character, followed by a new line.
- * @str: An input string
+ * puts2 - function for printing
+ * @str: A string
+ *
  * Return: 0
+ *
  */
 void puts2(char *str)
 {
-	int len = 0, i = 0;
+int len = 0, i;
 
-	while (str[len] != '\0')
-		len++;
-
-	len -= 1;
-
-	for (; i <= len; i += 2)
-		_putchar(str[i]);
-
-	_putchar('\n');
+while (str[len] != '\0')
+{
+	len++;
+}
+for (i = 0; i < len; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
