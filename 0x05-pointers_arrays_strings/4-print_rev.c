a6eca881aa5c0e5d5d2c2727f64f0e670c64313c
Miss24Chimboto <46428707+Miss24Chimboto@users.noreply.github.com>
2022-06-15 01:51:59 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,15 @@
  *
  * Return: 0
  */
-
 void print_rev(char *s)
 {
-s = "Printing in reverse";
-print_rev(s);
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len)
+		_putchar(s[--len]);
+
+	_putchar('\n');
 }
