fa8bc72363f695d1d00c0980333f761412211c2c
Miss24Chimboto <annastacia78@gmail.com>
2022-06-14 17:10:10 -0700

    half or strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,17 +11,17 @@ void puts_half(char *str)
 int len = 0, m, n;
 
 while (str[len] != '\0')
-	len++;
+len++;
 
 if (len % 2 == 0)
-	n = len / 2;
+n = len / 2;
 
 else
 
-	n = (len + 1) / 2;
+n = (len + 1) / 2;
 
 for (m = n; m < len; m++)
-       _putchar(str[m]);
+_putchar(str[m]);
 
 _putchar('\n');
 }
