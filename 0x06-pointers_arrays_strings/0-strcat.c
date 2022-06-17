b4e58daed5c3ae24bd834a8814a08ddbf3b89916
Miss24Chimboto <annastacia78@gmail.com>
2022-06-17 15:07:01 -0700

    Using _strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - A function that concatenates two strings
+ * @dest: represents a string
+ * @src: represents a string
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src);
+{
+int a, b;
+
+a = 0;
+b = 0;
+
+while (dest[a] != '\0')
+	a++;
+
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+
+return (dest);
+}
