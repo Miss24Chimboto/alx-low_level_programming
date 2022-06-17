e9a40c8b396d77caa1a614ecb8ec5e0de64a2e4d
Miss24Chimboto <annastacia78@gmail.com>
2022-06-17 16:44:27 -0700

    Changing the letter case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * string toupper - changing lowercase to uppercase
+ * @str: pointer
+ *
+ * Return: Char
+ */
+char *string_toupper(char *str)
+{
+char *ch = str;
+int i = 0;
+
+while (ch[i] != '\0')
+{
+if (ch[i] >= 'a' && ch[i] <= 'z')
+{
+ch[i] = ch[i] -32;
+}
+i++;
+}
+return (ch);
+}
