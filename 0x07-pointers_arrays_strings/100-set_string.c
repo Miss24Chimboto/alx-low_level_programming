ff7fba0db5726fe134abe8ffeace7c2f284638aa
Miss24Chimboto <annastacia78@gmail.com>
2022-06-22 13:22:33 -0700

    Double pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - Function that sets the string
+ * @s: double pointer parameter 
+ * @to: pointer parameter 
+ * Return: 0.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
