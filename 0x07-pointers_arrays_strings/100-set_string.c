19cc1a31058224e289b618576e4883a7ee30d26f
Miss24Chimboto <annastacia78@gmail.com>
2022-06-22 13:25:20 -0700

    removing the whitespaces

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * set_string - Function that sets the string
- * @s: double pointer parameter 
- * @to: pointer parameter 
+ * @s: double pointer parameter
+ * @to: pointer parameter
  * Return: 0.
  */
 void set_string(char **s, char *to)
