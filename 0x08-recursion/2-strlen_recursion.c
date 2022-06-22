0065955d623a0236e7ad01ca8ae5ea7bc609ee25
Miss24Chimboto <annastacia78@gmail.com>
2022-06-22 15:04:21 -0700

    len of *s

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a fx that returns the length of a string
+ * @: s - is a pointer string
+ *
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+
+	return (0);
+}
