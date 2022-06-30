537acdf1151e0f371a0bd82d0b78a1a7d3f2a325
Miss24Chimboto <annastacia78@gmail.com>
2022-06-30 14:04:02 -0700

    pointing with malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,38 +1,41 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- * _strdup - Function
- *
- * Description: contains a copy of a string parameter
- *
- * @str: parameter of type char, string to copy
- *
- * Return: returns pointer to newly allocated space
- * returns NULL if str is Null
+ *str_concat - Concatenates two strings
+ *@s1: First string
+ *@s2: Second String
+ *Return: Pointer to the new string
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	char *store;
-	unsigned int len = 0, x;
-
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	for (x = 0; str[x]; x++)
-	{
-		len++;
-	}
-	len++;
-	store = malloc(sizeof(char) * (len));
-	if (store == NULL)
-	{
-		return (NULL);
-	}
-	for (x = 0; str[x]; x++)
-	{
-		store[x] = str[x];
-	}
-	store[len] = '\0';
-	return (store);
-
+unsigned int i, j, k, l;
+char *s;
+if (s1 == NULL)
+{
+i = 0;
+}
+else
+{
+for (i = 0; s1[i]; i++)
+;
+}
+if (s2 == NULL)
+j = 0;
+else
+{
+for (j = 0; s2[j]; j++)
+;
+}
+k = i + j + 1;
+s = malloc(k *sizeof(char));
+if (s == NULL)
+return (NULL);
+for (l = 0; l < i; l++)
+{
+s[l] = s1[l];
+}
+for (l = 0; l < j; l++)
+s[l + i] = s2[l];
+s[i + j] = '\0';
+return (s);
 }
