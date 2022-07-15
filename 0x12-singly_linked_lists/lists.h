fea429337a94a2273eed2333a06cef040cb2e72c
Miss24Chimboto <annastacia78@gmail.com>
2022-07-15 02:40:54 -0700

    Fixing betty issues on prototype file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,10 +16,11 @@
  */
 typedef struct list_s
 {
-	    char *str;
-	        unsigned int len;
-		    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
+
 /*A function that prints all the elements of a list_t list*/
 size_t print_list(const list_t *h);
 
