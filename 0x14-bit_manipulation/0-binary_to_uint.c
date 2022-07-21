3b5d66c700e9df886cf4330ffc4b99db4c27fc5f
Miss24Chimboto <annastacia78@gmail.com>
2022-07-21 13:00:36 -0700

    Convert to binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointer to a string 
+ * @b: pointer to a string
  *
  * Return: unsigned int with decimal value of binary number
  */
