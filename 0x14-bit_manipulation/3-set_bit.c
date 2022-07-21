c1f585305ea078f5f77d3c5d57e06aae2d2298b3
Miss24Chimboto <annastacia78@gmail.com>
2022-07-21 13:21:52 -0700

    Set value of bit to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * set_bit - sets a bit at a given index to 1
- * @n: pointer to the number 
+ * @n: pointer to the number
  * @index: index of the bit to set to 1
  *
  * Return: 1 for success, -1 for failure
